dbfb8d533c35a5bd2a8ca856ced06ede08e564df
Ze1598 <jose.fernando.costa.1998@gmail.com>
2024-10-01 22:46:37 +0100

    Markdown updates with GitHub links

--- Home.py
+++ Home.py
@@ -15,6 +15,8 @@ st.markdown(
     This is a one stop-shop for all Golden Formulas documented by José Costa.
     Enjoy as a 26-year old from Portugal documents his journey and what he learns along the way 😊
 
-    Technical note: this web app is connected to his Notion workspace in real time meaning the content for these pages is dynamically generated via API calls,
+    Technical notes below 🗒️:
+    * This web app is connected to his Notion workspace in real time meaning the content for these pages is dynamically generated via API calls
+    * GitHub repository available [here](https://github.com/Ze1598/golden-formulas)
 """
 )
