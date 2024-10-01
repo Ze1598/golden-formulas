b1a3968acdb5e9fe7b133d1c649e242744bc05a4
Ze1598 <jose.fernando.costa.1998@gmail.com>
2024-10-01 22:09:34 +0100

    Test commit for streamlit deployment

---
+++ Home.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Home"
+)
+
+
+st.write("# Welcome to Golden Formulas! 👋")
+
+
+st.markdown(
+    """
+    This is a one stop-shop for all Golden Formulas documented by José Costa.
+    Enjoy as a 26-year old from Portugal documents his journey and what he learns along the way 😊
+
+    Technical note: this web app is directly connected to his Notion workspace meaning the content for these pages is dynamically generated via API calls - both the available pages and their content.
+"""
+)
---
+++ pages/Bouldering.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.markdown(
+    """
+    * Climb quickly without spending too long holding on to the same holds
+    * Increase finger strength
+    * Increase hip flexibility
+    * Get your hip close to the wall
+    * Climb with straight arms
+    * Generate momentum with your lower half
+"""
+)
